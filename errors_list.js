6ad4289960b1cd844f55d4fa4c1d733dcaf018ff
Tim Makken <t.makken@gmail.com>
2014-04-20 01:01:00 +0200

    Created Errors Package

---
+++ errors_list.js
@@ -0,0 +1,13 @@
+Template.meteorErrors.helpers({
+	errors: function() {
+	return Errors.collection.find();
+	}
+});
+
+Template.meteorError.rendered = function() {
+	var error = this.data;
+	
+	Meteor.defer(function() {
+	Errors.collection.update(error._id, {$set: {seen: true}});
+	});
+};
